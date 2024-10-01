f7eba4d4695802ff9868329d075fe6c7ddfa1f20
PiyushSharma31112 <piyushsharmadec009@gmail.com>
2024-10-01 10:55:24 +0530

    Features: Text

--- src/axios.js
+++ src/axios.js
@@ -5,9 +5,7 @@ const instance = axios.create({
   baseURL: "http://localhost:5050/api",
   timeout: 5000,
   headers: { Authorization: `Bearer ${token}` },
+  // hello
 });
 
 export default instance;
-
-
-// hello
