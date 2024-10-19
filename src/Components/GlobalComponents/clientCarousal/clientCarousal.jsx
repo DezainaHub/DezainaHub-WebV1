5de9271abc15a0314e00a4830f3ad98d0b4e85af
sourav0299 <105085605+sourav0299@users.noreply.github.com>
2024-10-19 21:14:58 +0530

    Feature: add tailwind to specific component

--- src/Components/GlobalComponents/clientCarousal/clientCarousal.jsx
+++ src/Components/GlobalComponents/clientCarousal/clientCarousal.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import '../clientCarousal/clientCarousal.css'
 
 const clientCarousal = () => {
   return (
-    <div>clientCarousal</div>
+    <div className='bg-black text-[B5B8B8] h-[176px] flex items-center font-manrope'>
+      <div className="bg-black px-40 text-2xl">Trusted By</div>
+      
+    </div>
   )
 }
 
